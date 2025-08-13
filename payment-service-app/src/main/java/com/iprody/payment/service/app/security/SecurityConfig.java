bd7d59ce04b6c7c71d030bbfd785849b8ba12241
tiksofiya02 <tiksofiya02@gmail.com>
2025-08-13 09:36:35 +0200

    homework-13

---
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.iprody.payment.service.app.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/payments/**")
+                        .hasAnyRole("reader", "admin")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt ->
+                                jwt.jwtAuthenticationConverter(jwtConverter))
+
+                );
+        return http.build();
+    }
+
+}
