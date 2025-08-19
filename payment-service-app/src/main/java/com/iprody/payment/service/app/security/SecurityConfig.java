7ecc373f6b7a483d7df0ce73b00bebf665060d29
tiksofiya02 <tiksofiya02@gmail.com>
2025-08-19 08:41:52 +0200

    disable http session

--- payment-service-app/src/main/java/com/iprody/payment/service/app/security/SecurityConfig.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
@@ -17,6 +18,7 @@ public class SecurityConfig {
         jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
 
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/payments/**")
                         .hasAnyRole("reader", "admin")
